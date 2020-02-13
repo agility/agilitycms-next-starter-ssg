031fd549c05142fca980982fa50fd8d12a136462
James Vidler <james@agilitycms.com>
2020-02-13 17:54:20 -0500

    added shell script for uploading to azure blob

---
+++ az.storage.blob.upload-batch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# A simple Azure Storage example script
+
+resourceGroup=$1
+storageAccount=$2
+sourceDir=$3
+
+
+echo "Retrieving storage account key..."
+storageAccountKey=$(az storage account keys list -g $resourceGroup -n $storageAccount --query [0].value -o tsv)
+
+echo "Setting storage context..."
+export AZURE_STORAGE_ACCOUNT=$storageAccount
+export AZURE_STORAGE_KEY=$storageAccountKey
+
+echo "Uploading /$sourceDir directory to $storageAccount static website container..."
+az storage blob upload-batch -d '$web' -s $sourceDir
+
+echo "Done!"
