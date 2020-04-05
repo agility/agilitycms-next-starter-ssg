00fc7d8270709a513b2b11a96bf88e6bdd66c400
jamesvidler <james.vidler@outlook.com>
2020-04-05 14:41:53 -0400

    added router check to support fallback

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,18 @@
 import PreviewBar from './PreviewBar'
 import GlobalHeader from './GlobalHeader'
+import { useRouter } from 'next/router'
 
 export default function Layout(props) {
 
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  const router = useRouter()
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
   //BUG: when dynamic imports used in this case, the HTML does not output SSR
   //const AgilityPageTemplate = dynamic(() => import ('../pageTemplates/' + props.pageTemplateName));
-
   const AgilityPageTemplate = require('../pageTemplates/' + props.pageTemplateName + '.js').default;
 
   return (
